308f75807bc421079ac9be52b9b12c66e381de27
Atıl Samancıoğlu <samancioglu.atil@gmail.com>
2021-08-17 13:05:17 +0300

    async await features added

--- CryptoCrazySwiftUI/ViewModel/CryptoViewModel.swift
+++ CryptoCrazySwiftUI/ViewModel/CryptoViewModel.swift
@@ -13,6 +13,18 @@ class CryptoListViewModel : ObservableObject {
     
     let webservice = Webservice()
     
+    func downloadCryptosAsync(url : URL) async {
+        do {
+        let cryptos = try await webservice.downloadCurrenciesAsync(url: url)
+            DispatchQueue.main.async {
+                self.cryptoList = cryptos.map(CryptoViewModel.init)
+            }
+        } catch {
+            print(error)
+        }
+    }
+    
+    /*
     func downloadCryptos(url : URL) {
         webservice.downloadCurrencies(url: url, completion: { result in
             switch result {
@@ -27,6 +39,7 @@ class CryptoListViewModel : ObservableObject {
             }
         })
     }
+     */
 }
 
 struct CryptoViewModel {
